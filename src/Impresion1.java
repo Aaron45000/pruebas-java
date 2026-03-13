28f029d0c903d8221beab459a992f6aa5b7e25c1
Aaron45000 <aaronalejandro4500@gmail.com>
2026-03-13 18:11:12 -0300

    Primer Commit Rama-1

---
+++ src/Impresion1.java
@@ -0,0 +1,11 @@
+public class Impresion1
+{
+
+    public static void Imprimir_1()
+    {
+
+        System.out.println("Impresion Rama 1");
+
+    }
+
+}
--- src/Impresiones.java
+++ src/Impresiones.java
@@ -1,12 +1,12 @@
-
-
-
 public class Impresiones
 {
 
     public static void main(String[] args)
     {
-        System.out.println("Primera Impresion 1!1!!");
+
+        System.out.println("Primera Impresion 1!1!");
+        Impresion1.Imprimir_1();
+
 
     }
 
