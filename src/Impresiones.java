b3b3a3296b74984d04a6ca312b59aa0a25535fe1
Aaron45000 <aaronalejandro4500@gmail.com>
2026-03-13 18:36:06 -0300

    Rama 2

--- src/Impresiones.java
+++ src/Impresiones.java
@@ -1,16 +1,7 @@
+void main() {
 
 
-
-public class Impresiones
-{
-
-    public static void main(String[] args)
-    {
-
-
-        System.out.println("Primera Impresion 1!1!");
-        Rama2.Imprimo2();
-
-    }
+    IO.println("Primera Impresion 1!1!");
+    Rama2.Imprimo2();
 
 }
